d1c0c62d33665ca2992b9c3fd41c191e01509eb9
Denys Marytchak <denys.marytchak@gmail.com>
2020-02-20 01:48:41 +0100

    fix: avoid condition

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 module.exports = function reverse (n) {
-    if (n<0) return Math.abs(n).toString().split("").reverse().join("")
-    else return n.toString().split("").reverse().join("");
+    return Math.abs(n).toString().split("").reverse().join("");
 }
